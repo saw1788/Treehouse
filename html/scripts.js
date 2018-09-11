9bf7bd50ccd95dc5bdfe2a153e0d100c10dd1242
saw1788 <saw1788@gmail.com>
2018-09-11 18:26:08 -0400

    Added some js, still thinking about css.

---
+++ html/scripts.js
@@ -0,0 +1,40 @@
+function isBackpackFull(startingItems){
+    var numItems = 0;
+
+    if (document.getElementById("sword").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("staff").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("bow").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("arrows").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("torch").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("rope").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("cloak").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("canteen").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("rations").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("spellBook").checked){
+        numItems += 1;
+    }
+    if (document.getElementById("lockPick").checked){
+        numItems += 1;
+    }
+    if (numItems > 5) {
+        alert('Your backpack is full.')
+    }
+}
